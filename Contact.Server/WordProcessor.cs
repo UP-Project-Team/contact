5b70cf1c6b0b9b6ae5a171dba98e06572937cc4c
ephemere <nastenok1992@gmail.com>
2012-12-01 01:33:14 +0700

    Changes at WordProcessor.cs

--- Contact.Server/WordProcessor.cs
+++ Contact.Server/WordProcessor.cs
@@ -8,5 +8,36 @@ namespace Contact.Server
 {
     class WordProcessor
     {
+        public string PrimaryWord;
+        public string CurrentWord;
+        public string VarOfCurWord;
+        public int CountOfOpenChars;
+        public List<string> UsedWords = new List<string>();
+        public string ChiefWord;
+        public string Question;
+        public List<char> LettersOfPrimaryWord = new List<char>();
+
+
+        public bool CheckUsedWords()
+        {
+            foreach (string a in UsedWords)
+            {
+                if (CurrentWord == a)
+                    return true;
+            }
+            return false;
+        }
+
+        public void AddToTheOpenChars(char a)
+        {
+            LettersOfPrimaryWord.Add(a);
+        }
+
+        public bool CompareWithPrimaryWord()
+        {
+            if ((CurrentWord == PrimaryWord) && (VarOfCurWord == PrimaryWord))
+                return true;
+            return false;
+        }
     }
 }
